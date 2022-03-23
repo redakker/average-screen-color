8e9a39baec81d2451cc0d33fd2f64f06f389ff3c
redakker <redman@redman.hu>
2022-03-23 22:01:29 +0100

    Implement MQTT message

--- average-screen-color.py
+++ average-screen-color.py
@@ -1,18 +1,39 @@
 # Author: redakker 
 # email: redman at redman dot hu
 
-from PIL import ImageGrab
+from PIL import ImageGrab # pip install pillow
 import time
 import os
-from colour import Color
+import math 
+from colour import Color # pip install colour
+import paho.mqtt.client as mqtt # pip install paho-mqtt
+import logging
+from config import Config
+
+config = Config();
+logging.basicConfig(filename=config.logfile,level=logging.DEBUG,format='%(asctime)s %(message)s');
 
 
 # Config
-LOOP_INTERVAL  = 3    # how often we calculate screen colour (in seconds)
-DURATION       = 3    # how long it takes bulb to switch colours (in seconds)
+LOOP_INTERVAL  = 1    # how often we calculate screen colour (in seconds)
 SKIP_PIXELS       = 10   # skip every SKIP_PIXELS number of pixels to speed up calculation
 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    logging.debug("Connected with result code "+str(rc))
+	
 
+
+client = mqtt.Client()
+client.will_set(config.baseTopic, "0,0,0")
+client.on_connect = on_connect
+
+
+#Set userid and password
+client.username_pw_set(config.user, config.password)
+
+#Connect
+client.connect(config.broker, config.port, config.keepalive)
 
 
 # run loop
@@ -45,6 +66,15 @@ while True:
 	#print ("\n average   red:%s green:%s blue:%s" % (red,green,blue))	
 	print ("\n average  (hex) "+  (c.hex))
 	
+	r = (math.floor(c.get_red() * 255))
+	g = (math.floor(c.get_green() * 255))
+	b = (math.floor(c.get_blue() * 255))
+
+	rgb = "" + str(r) + "," + str(g) + "," + str(b)
+
+	 
+
+	client.publish(config.baseTopic,rgb)
 	# TODO
     # Send an MQTT command with the Hex value
     # and/or
